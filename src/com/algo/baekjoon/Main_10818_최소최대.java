e39d4cca9e314aa83b84fc182e14c68f31e09804
송복민 <bockmin@gmail.com>
2023-04-08 22:45:27 +0900

    Main_10818_최소최대

---
+++ src/com/algo/baekjoon/Main_10818_최소최대.java
@@ -0,0 +1,25 @@
+package com.ssafy.algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_10818_최소최대 {
+
+	static int N;
+	static int[] arr;
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[0]+" "+arr[N-1]);
+	}
+}
